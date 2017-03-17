68d17b56d7deb805ac5419de7d86fcbcd1ea1f9d
alex <aaa123qqq@mail.ru>
2017-03-17 09:31:38 +0300

    add 112A task

---
+++ 112a.rb
@@ -0,0 +1,18 @@
+n = gets.chomp.downcase
+m = gets.chomp.downcase
+a = 0
+
+if n == m
+  puts "0"
+else
+  while n[a] == m[a]
+    a += 1
+  end
+
+  if n[a] < m[a]
+    puts "-1"
+  elsif n[a] > m[a]
+    puts "1"
+  end
+end
+
